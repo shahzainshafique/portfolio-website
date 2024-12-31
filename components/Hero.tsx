32200599e8c818804758a938a3c1eca6bb0b61dd
Shahzain Shafique <shahzainshafique1@yahoo.com>
2024-12-31 15:42:11 +0500

    Hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -26,9 +26,6 @@ const Hero = () => {
       </div>
       <div className="flex justify-center relative my-20 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
-          <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
-            Welcome to the future of the web
-          </h2>
           <TextGenerateEffect
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
             words="Transforming Concepts into Seamless User Experience"
