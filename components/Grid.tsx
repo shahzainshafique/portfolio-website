a62210d176ff9522a29f0ec11a367d2a97d7e046
Shahzain Shafique <shahzainshafique1@yahoo.com>
2025-01-07 14:00:43 +0500

    Static files upload

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,23 +1,14 @@
 import React from 'react'
 import { BentoGrid, BentoGridItem } from './ui/bento-grid'
+import { gridItems } from '@/data'
 
 const Grid = () => {
   return (
     <section id='about'>
-        <BentoGrid>
-          <BentoGridItem
-            title='About'
-            description='I am a software engineer based in Pakistan. I specialize in building dynamic web applications with Next.js.'
-          />
-          <BentoGridItem
-            title='Projects'
-            description='I have worked on a variety of projects ranging from e-commerce websites to social media platforms.'
-          />
-          <BentoGridItem
-            title='Contact'
-            description='Feel free to reach out to me if you have any questions or would like to collaborate.'
-          />
-          </BentoGrid>
+        <BentoGrid>{gridItems.map(({id,title,description,className,img,imgClassName,titleClassName,spareImg})=>(
+            <BentoGridItem key={id} id={id} title={title} description={description} className={className} img={img} imgClassName={imgClassName} titleClassName={titleClassName} spareImg={spareImg} />
+        ))}</BentoGrid>
+          
     </section>
   )
 }
